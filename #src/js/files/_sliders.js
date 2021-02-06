b10e214909e17fc81ba4a7e2b85c3418c9bb5bb3
IuriiKyrylovskyi <iurii712@ukr.net>
2021-02-06 03:18:52 +0200

    constructions sliders

--- #src/js/files/_sliders.js
+++ #src/js/files/_sliders.js
@@ -45,20 +45,51 @@ if ($('.constructions__slider-big').length > 0) {
 
 if ($('.constructions__slider-small').length > 0) {
   $('.constructions__slider-small').slick({
-    // vertical: true,
+    vertical: true,
+    centerMode: true,
+    centerPadding: '30px',
+
     slidesToShow: 4,
     slidesToScroll: 1,
-    arrows: false,
     // adaptiveHeight: true,
     variableWidth: true,
-    centerMode: true,
     focusOnSelect: true,
-    useTransform: false,
+    verticalSwiping: true,
+    // useTransform: false,
     // rtl: true,
-    // dots: true,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
+
+    arrows: false,
     infinite: true,
     asNavFor: '.constructions__slider-big',
   });
 }
+
+// $(window).on('resize orientationchange', function () {
+//   if ($(window).width() > 1200) {
+//     $('.slider-nav').slick('unslick');
+//     $('.slider-nav').slick({
+//       slidesToShow: 4,
+//       asNavFor: '.slider-main',
+//       vertical: true,
+//       focusOnSelect: true,
+//       autoplay: false,
+//       centerMode: true,
+//     });
+//   }
+// });
+
+// $(document).ready(function () {
+//   $('.slick-slider').slick({
+//     vertical: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     arrows: true,
+//     prevArrow: '<div class="slick-prev"></div>',
+//     nextArrow: '<div class="slick-next"></div>',
+//     dots: true,
+//     customPaging: function (slider, i) {
+//       return '<a href="#"><img src="images/slide-dot.png" /><img src="images/slide-dot-active.png" /></a>';
+//     },
+//   });
+// });
