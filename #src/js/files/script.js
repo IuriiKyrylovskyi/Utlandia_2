f6bad4a27b89a224eb5aad43d4ecdc5bcf30393b
IuriiKyrylovskyi <iurii712@ukr.net>
2021-02-22 15:37:34 +0200

    radio

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -18,3 +18,16 @@ $('.licence__imgs-box').niceScroll({
 });
 // parallax
 new universalParallax().init();
+	//RADIO
+	$.each($('.radio__item'), function(index, val) {
+		if($(this).find('input').prop('checked')==true){
+			$(this).addClass('_active');
+		}
+	});
+	$(document).on('click', '.radio__item', function(event) {
+		$(this).parents('.radio').find('.radio__item').removeClass('_active');
+		$(this).parents('.radio').find('.radio__item input').prop('checked',false);
+		$(this).toggleClass('_active');
+		$(this).find('input').prop('checked',true);
+		return false;
+	});
--- Utlandia_2/js/app.js
+++ Utlandia_2/js/app.js
@@ -3873,7 +3873,20 @@ $('.licence__imgs-box').niceScroll({
   cursorcolor: '#003DD6',
 });
 // parallax
-new universalParallax().init();;
+new universalParallax().init();
+	//RADIO
+	$.each($('.radio__item'), function(index, val) {
+		if($(this).find('input').prop('checked')==true){
+			$(this).addClass('_active');
+		}
+	});
+	$(document).on('click', '.radio__item', function(event) {
+		$(this).parents('.radio').find('.radio__item').removeClass('_active');
+		$(this).parents('.radio').find('.radio__item input').prop('checked',false);
+		$(this).toggleClass('_active');
+		$(this).find('input').prop('checked',true);
+		return false;
+	});;
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf('MSIE ');
 var isMobile = {
