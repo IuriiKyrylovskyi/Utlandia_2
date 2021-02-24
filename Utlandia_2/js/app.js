73f0c39fe99963eed88ec98d54ebe243e1b48a0d
IuriiKyrylovskyi <iurii712@ukr.net>
2021-02-25 01:52:02 +0200

    input main8

--- Utlandia_2/js/app.js
+++ Utlandia_2/js/app.js
@@ -3874,19 +3874,28 @@ $('.licence__imgs-box').niceScroll({
 });
 // parallax
 new universalParallax().init();
-	//RADIO
-	$.each($('.radio__item'), function(index, val) {
-		if($(this).find('input').prop('checked')==true){
-			$(this).addClass('_active');
-		}
-	});
-	$(document).on('click', '.radio__item', function(event) {
-		$(this).parents('.radio').find('.radio__item').removeClass('_active');
-		$(this).parents('.radio').find('.radio__item input').prop('checked',false);
-		$(this).toggleClass('_active');
-		$(this).find('input').prop('checked',true);
-		return false;
-	});;
+//RADIO
+$.each($('.radio__item'), function (index, val) {
+  if ($(this).find('input').prop('checked') == true) {
+    $(this).addClass('_active');
+  }
+});
+$(document).on('click', '.radio__item', function (event) {
+  $(this).parents('.radio').find('.radio__item').removeClass('_active');
+  $(this).parents('.radio').find('.radio__item input').prop('checked', false);
+  $(this).toggleClass('_active');
+  $(this).find('input').prop('checked', true);
+  return false;
+});
+
+// tel mask
+var elements = document.getElementsByClassName('imaskjs__input_tel');
+for (var i = 0; i < elements.length; i++) {
+  new IMask(elements[i], {
+    mask: '+{38}(000)000-00-00',
+  });
+}
+;
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf('MSIE ');
 var isMobile = {
